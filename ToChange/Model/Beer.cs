54926dcf5cb274e933a905888dec074d2ff88188
Krystian Bujak <Booyac@MacBook-Pro-macbook.local>
2018-11-21 18:11:01 +0100

    Initial commit

---
+++ ToChange/Model/Beer.cs
@@ -0,0 +1,17 @@
+﻿namespace ETL.Model
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Percentages { get; set; }
+
+        public int Blg { get; set; }
+
+        public string Country { get; set; }
+
+        public double Rating { get; set; }
+    }    
+}
---
+++ ToChange/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace ETL
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            var m = new BeerRankingWebsiteCrawler();
+            m.startCrawlerAsync().Wait();
+            Console.WriteLine(m.beerList.Count());
+            Console.Read();
+        }
+
+
+    }
+}
